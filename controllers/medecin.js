9e38deae574a41c8f28d0ebe5cdce20fff54f7b8
fouadesi <83358349+djellalifouad@users.noreply.github.com>
2021-07-13 21:42:21 +0100

    medicalfile-interface-

--- controllers/medecin.js
+++ controllers/medecin.js
@@ -1,9 +1,45 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const db = require("../util/db").db;
+
+exports.getMedicalFile = (req,res,next)=> {
+  
+  console.log(req.query.id);
+  db.query("Select * from Patient,personalhistory where Patient.IdPatient = ? and personalhistory.IdPatient = ? ",
+  [req.query.id,req.query.id],
+  (err,result1)=> {
+    db.query("Select * from haveintoxication where haveintoxication.Idpersonalhistory = ?  ",
+    [result1[0].Idpersonalhistory],
+    (errors,result2)=> {
+      db.query("Select * from medicalsurgicalhistory where medicalsurgicalhistory.IdPatient = ?", 
+      [req.query.id,],(err,result3) => {
+
+
+        res.render("medicalfile/medicalFile",
+          {
+            dataB : result1[0] ,
+            
+            dataC: result2,
+          }
+        
+        );
+      }
+
+      )
+      
+    }
+    )
+
+  } );
+
+}
+exports.logout = (req,res,next)=> {
+  res.clearCookie("jwt");
+ res.send({
+   "message" : "Disconnected" 
+ })
+}
 exports.getList = (req, res, next) => {
-
-
   if(req.headers.cookie == null ) {
     res.redirect('/users/login');
     return; 
@@ -19,7 +55,7 @@ exports.getList = (req, res, next) => {
     (err,decodedToken)=> {
       console.log(decodedToken);
     
-   if (decodedToken.role == "médecin") {
+   if (decodedToken.role == "medecin") {
       // medecin home provisoire ; 
       db.query("Select * from patient",(err,result)=> {
         if(err) {
