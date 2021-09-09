1b71a31ecae728c629d94bbaac06b8885b003189
bendjeddoucheima <85156987+bendjeddoucheima@users.noreply.github.com>
2021-09-09 14:59:39 -0700

    examen medical
    examen medical

--- routes/users/medecin.js
+++ routes/users/medecin.js
@@ -1,10 +1,97 @@
 const express = require('express')
 const medecinControler  = require("../../controllers/medecin"); 
-const router =express.Router()
-
-
+const medecinControler2  = require("../../controllers/medecin2"); 
+const medecinControler3  = require("../../controllers/medecin3"); 
+const medecinControler4  = require("../../controllers/medecin4"); 
+const medecinControler5  = require("../../controllers/medecin5"); 
+const router =express.Router();
 
 router.get('/list',medecinControler.getList);
 router.get('/medicalFile',medecinControler.getMedicalFile); 
+router.get('/medicalfileUpdate',medecinControler.getUpdateMedicalFile);
+router.get('/medicalexam' , medecinControler.getExam);
+router.post('/addIntoxication', medecinControler.postIntoxication); 
+router.post('/deleteIntoxication', medecinControler.deleteIntoxication); 
+router.post('/updatePersonalHistory', medecinControler.updatePersonalHistory);
+router.post('/addAffection', medecinControler.addAffection);
+router.post('/deleteAffection',medecinControler.deleteAffection);
+router.post('/addMaladie',medecinControler.addMaladie);
+router.post('/deleteMaladie',medecinControler.deleteMaladie);
+router.post('/addAllergy',medecinControler.addAlergie);
+router.post('/deleteAllergie',medecinControler.deleteAllergie);
+router.get('/getExamen',medecinControler.getMedicalExam);
+router.get('/profile',medecinControler.getProfile);
+router.get('/getExam',medecinControler.getExam);
+router.post('/changePictureFile',medecinControler.changePictureFile);
+router.post('/addWork',medecinControler2.addWork);
+router.post('/deleteWork',medecinControler2.deleteWork);
+router.post('/changePassword',medecinControler2.changePassword);
+router.post('/makePDFexam', medecinControler.postExamenMedical);
+router.post('/editExam', medecinControler.postEditExam);
+router.post('/deleteExamFile',medecinControler.deleteExamFile);
+
+
+
+//examen medical
+router.get("/infoExam",medecinControler3.getinfoExam);
+router.get("/infoUpdateExam",medecinControler3.getinfoUpdateExam);
+router.post("/addExam",medecinControler3.postaddExam);
+router.post("/updateExam",medecinControler3.postupdateExam);
+router.post("/addSickness",medecinControler3.postaddSickness);
+
+
+
+//examen complemantaire
+router.post("/addMedicament",medecinControler3.postMedicament);
+router.post("/addExamen",medecinControler3.postExamen);
+
+router.post("/addSicknote",medecinControler3.postSicknote);
+router.post("/addOrientation",medecinControler3.postOrientation);
+router.post("/addBilan",medecinControler3.postBilan);
+router.post("/addEvacuation",medecinControler3.postEvacuation);
+router.post("/addCertificatRepos",medecinControler3.postCertificatRepos);
+router.post("/addCertificatPratique",medecinControler3.postCertificatPratique);
+
+router.post("/dataSicknote",medecinControler3.postdataSicknote);
+router.post("/dataBilan",medecinControler3.postdataBilan);
+router.post("/dataOrientation",medecinControler3.postdataOrientation);
+router.post("/dataEvacuation",medecinControler3.postdataEvacuation);
+router.post("/dataCertificat",medecinControler3.postdataCertificat);
+
+router.post("/updateSicknote",medecinControler3.postupdateSicknote);
+router.post("/updateOrientation",medecinControler3.postupdateOrientation);
+router.post("/updateBilan",medecinControler3.postupdateBilan);
+router.post("/updateEvacuation",medecinControler3.postupdateEvacuation);
+router.post("/updateCertificatRepos",medecinControler3.postupdateCertificatRepos);
+router.post("/updateCertificatPratique",medecinControler3.postupdateCertificatPratique);
+
+router.post("/deleteSicknote",medecinControler3.postdeleteSicknote);
+router.post("/deleteOrientation",medecinControler3.postdeleteOrientation);
+router.post("/deleteBilan",medecinControler3.postdeleteBilan);
+router.post("/deleteEvacuation",medecinControler3.postdeleteEvacuation);
+router.post("/deleteCertificat",medecinControler3.postdeleteCertificat);
+
+router.post("/imprimeSicknote",medecinControler3.postimprimeSicknote);
+router.post("/imprimeBilan",medecinControler3.postimprimeBilan);
+router.post("/imprimeOrientation",medecinControler3.postimprimeOrientation);
+router.post("/imprimeEvacuation",medecinControler3.postimprimeEvacuation);
+router.post("/imprimeCertificat",medecinControler3.postimprimeCertificat);
+
+//======================RDV==============================
+router.get('/RDV', medecinControler4.getRDV);
+router.post('/makeRDV', medecinControler4.postRDV);
+router.post('/dataupdaterdv', medecinControler4.postdataupdaterdv);
+router.post('/updaterdv', medecinControler4.postupdaterdv);
+
+//promo interface
+router.get('/RDVpromo', medecinControler.getRDVpromo);
+router.post('/makeRDVpromo', medecinControler.postRDVpromo);
+router.post('/editRDVpromo', medecinControler.editRDVpromo);
+router.post('/cancelRDVpromo', medecinControler.cancelRDVpromo);
+
+
+//======================STATIC==============================
+router.get('/infoStat', medecinControler5.getStat);
+
 
 module.exports = router ; 
